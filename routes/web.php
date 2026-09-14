2f2fa24ab1e6e0a5d5049f99ef052a8494199f02
maungkyawkyawpyae12-ux <maungkyawkyawpyae12@gmail.com>
2026-09-14 07:25:25 +0630

    third

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[App\Http\Controllers\FrontController::class,'shop'])->name('shop');
 
 Auth::routes();
 
